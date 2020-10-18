11aeb21bb1339b8542a0296c97da15419d1d78e6
mgblackwater <mgblackwater@gmail.com>
2020-10-18 20:48:04 +0800

    Updates

---
+++ precache-manifest.c98583a8a97a3f83d01c8c28d788f02a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e05e6121c8dd7591062e1d1159348f",
+    "url": "/plant-book/index.html"
+  },
+  {
+    "revision": "74287e6bdb9451024385",
+    "url": "/plant-book/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93e92ab477bb2bf36b58",
+    "url": "/plant-book/static/js/2.f68297fd.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/plant-book/static/js/2.f68297fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74287e6bdb9451024385",
+    "url": "/plant-book/static/js/main.72c93ddf.chunk.js"
+  },
+  {
+    "revision": "ce9e6cee4869f7ba927c",
+    "url": "/plant-book/static/js/runtime-main.a257bdf1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/plant-book/static/media/logo.5d5d9eef.svg"
+  }
+]);
