c89adb917178ba3808d2c41b7d945ac9a33f39e4
Kyaw Nyi Win <KyawNyiWin@medinno.onmicrosoft.com>
2020-10-21 19:34:28 +0800

    plant-book

--- src/App.js
+++ src/App.js
@@ -1,52 +1,136 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import PlantContainer from "./plant-components";
+import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import {
+  AppBar,
+  Paper,
+  Card,
+  TextField,
+  CardMedia,
+  CssBaseline,
+  Icon,
+  Grid,
+} from '@material-ui/core'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
+import background from './static/background.jpg'
+import './App.css'
 
-class plant {
-  constructor(species, imageUrl, leafColor, lifeSpan) {
-    this.species = species;
-    this.imageUrl = imageUrl;
-    this.leafColor = leafColor;
-    this.lifeSpan = lifeSpan;
-  }
-}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    minHeight: '100vh',
+    backgroundImage: `url(${background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+  },
+  content: {},
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: 10,
+    width: '50%',
+    position: 'fixed' /* or absolute */,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  button: {marginLeft: 5},
+  textField: {color: theme.palette.primary},
+}))
 
 function App() {
-  const plantA = new plant(
-    "Moonlight Potho",
-    "https://images.squarespace-cdn.com/content/v1/59480bc7725e25bf7ae3ade9/1499276424141-L2POPWFQ7SR1JM0TYMOJ/ke17ZwdGBToddI8pDm48kMJE_oPqVDlYco0BmZZqKB17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1URi4X8B9Krz2qQ9LtOQvf9tzTXvYGV_EBNca-TfE3VhzzrD36oouzk3SSBf3bbVhnA/Moonlight+6+inch.jpg?format=2500w",
-    "green",
-    "3 years"
-  );
-  const plantB = new plant(
-    "Montreal",
-    "https://img.crocdn.co.uk/images/products2/pl/20/00/03/20/pl2000032008.jpg?width=940&height=940",
-    "Dark green",
-    "5 years"
-  );
-  const plantC = new plant(
-    "Purple Orchid",
-    "https://s3.amazonaws.com/finegardening.s3.tauntoncloud.com/app/uploads/2018/01/23173930/041082036-01_xlg.jpg",
-    "Purple Flower, Green leaves",
-    "1 years"
-  );
-  const plants = [plantC, plantA, plantB];
+  const classes = useStyles()
+  const [auth, setAuth] = React.useState(true)
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked)
+  }
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
-    <div className="App">
-      <div
-        style={{
-          width: "1024px",
-          margin: "0 auto",
-        }}
-      >
-        <h1>My Favorite Plants</h1>
-        {plants.map((plant) => (
-          <PlantContainer plant={plant} />
-        ))}
+    <>
+      <CssBaseline></CssBaseline>
+      <div className={classes.root}>
+        <AppBar>
+          <Toolbar>
+            <IconButton
+              edge='start'
+              className={classes.menuButton}
+              color='inherit'
+              aria-label='menu'
+              onClick={handleMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorEl}
+              anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+              transformOrigin={{vertical: 'top', horizontal: 'center'}}
+              keepMounted
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Categories</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+            <Typography variant='h6' className={classes.title}>
+              Plant Book
+            </Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.content}>
+          <Paper elevation={3} square className={classes.paper}>
+            <Grid
+              container
+              direction='row'
+              justify='center'
+              alignItems='center'
+            >
+              <Grid item xs={12} md={10}>
+                <TextField
+                  color='primary'
+                  className={classes.textField}
+                  fullWidth
+                  id='search'
+                />
+              </Grid>
+              <Grid item xs={12} md={2}>
+                <Button
+                  color='primary'
+                  variant='outlined'
+                  fullWidth
+                  size='medium'
+                  className={classes.button}
+                >
+                  Search Plant
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </div>
       </div>
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createMuiTheme} from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {ThemeProvider} from '@material-ui/styles'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      light: '#5efc82',
+      main: '#00c853',
+      dark: '#009624',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#ffe54c',
+      main: '#ffb300',
+      dark: '#c68400',
+      contrastText: '#000000',
+    },
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
